72526283a3f5571b660b850ae894d7f2613df72f
JoaoDias <joaodiasworking@gmail.com>
2022-08-28 21:51:40 -0300

    Adicionando ReturnValue

---
+++ Python Short-Term Immersion Google/Week2/ReturnValue.py
@@ -0,0 +1,10 @@
+# Use the get_seconds function to calculate the amount of 2 hours and 30 minutes of seconds, and then add this number to the amount of seconds of 45 minutes and 15 seconds. Then print the results.
+
+def get_seconds(hours, minutes, seconds):
+    return 3600*hours + 60*minutes + seconds
+
+
+amount_a = get_seconds(___)
+amount_b = get_seconds(___)
+result = ___
+print(result)
